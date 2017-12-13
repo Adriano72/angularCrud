fd3f908c80e2e8a36da8584dbdf9ea461ba9169a
Adriano Massi <adrianomassi@gmail.com>
2017-12-13 01:24:55 +0100

    Randomusers e styling

--- js/httpCalls.js
+++ js/httpCalls.js
@@ -4,7 +4,7 @@ var app = angular.module('servizi', []);
 app.factory("getLista", function($http){
   return {
     fetchData: function(){
-      return $http.get("http://jsonplaceholder.typicode.com/posts");
+      return $http.get("https://randomuser.me/api/?results=500&nat=gb");
     }
   }
 });
